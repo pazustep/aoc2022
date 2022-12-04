2daf56d12d9327ccbf6c125011add90476ff669c
Marcus Brito <marcus@bri.to>
2022-12-04 14:48:06 -0300

    Day 1, Part 1

---
+++ day1/src/main.rs
@@ -0,0 +1,17 @@
+use itertools::Itertools;
+
+fn main() {
+    let max = include_str!("input.txt")
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .max();
+
+    println!("{max:?}");
+}
