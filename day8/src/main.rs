ddc0e4bc4427dd6648ab2d7daf7339ee7f08e08e
Marcus Brito <marcus@bri.to>
2022-12-08 10:46:51 -0300

    Add more structure to the solution
    The algorithm is still just as naïve

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,76 +1,97 @@
 fn main() {
     let input = include_str!("input.txt");
-    let grid: Vec<Vec<u8>> = input
-        .lines()
-        .map(|line| line.bytes().map(|byte| byte - b'0').collect())
-        .collect();
+    let forest = Forest::parse(input);
 
-    let mut total_count = 0;
-    for y in 0..grid.len() {
-        let row = grid.get(y).unwrap();
+    let visible_count = forest
+        .trees()
+        .filter(|tree| forest.is_visible(tree))
+        .count();
 
-        for x in 0..row.len() {
-            if is_visible(&grid, x, y) {
-                total_count += 1;
+    println!("number of visible trees: {visible_count}");
+}
+
+struct Tree {
+    x: usize,
+    y: usize,
+    height: u8,
+}
+
+struct Forest {
+    trees: Vec<Vec<u8>>,
+}
+
+impl Forest {
+    fn parse(input: &str) -> Self {
+        let trees = input
+            .lines()
+            .map(|line| line.bytes().map(|byte| byte - b'0').collect())
+            .collect();
+
+        Self { trees }
+    }
+
+    fn trees(&self) -> impl Iterator<Item = Tree> + '_ {
+        self.trees.iter().enumerate().flat_map(move |(y, row)| {
+            row.iter().enumerate().map(move |(x, height)| Tree {
+                x,
+                y,
+                height: *height,
+            })
+        })
+    }
+
+    fn is_visible(&self, tree: &Tree) -> bool {
+        self.is_visible_from_left(tree)
+            || self.is_visible_from_right(tree)
+            || self.is_visible_from_top(tree)
+            || self.is_visible_from_bottom(tree)
+    }
+
+    fn is_visible_from_top(&self, tree: &Tree) -> bool {
+        for iy in 0..tree.y {
+            let height = self.trees.get(iy).unwrap().get(tree.x).unwrap();
+            if *height >= tree.height {
+                return false;
             }
         }
+
+        true
     }
 
-    println!("number of visible trees: {total_count}");
-}
-
-fn is_visible(grid: &[Vec<u8>], x: usize, y: usize) -> bool {
-    let subject_height = *grid.get(y).unwrap().get(x).unwrap();
-    is_visible_from_left(grid, x, y, subject_height)
-        || is_visible_from_right(grid, x, y, subject_height)
-        || is_visible_from_top(grid, x, y, subject_height)
-        || is_visible_from_bottom(grid, x, y, subject_height)
-}
-
-fn is_visible_from_top(grid: &[Vec<u8>], x: usize, y: usize, subject_height: u8) -> bool {
-    for iy in 0..y {
-        let height = grid.get(iy).unwrap().get(x).unwrap();
-        if *height >= subject_height {
-            return false;
+    fn is_visible_from_bottom(&self, tree: &Tree) -> bool {
+        for iy in (tree.y + 1)..(self.trees.len()) {
+            let height = self.trees.get(iy).unwrap().get(tree.x).unwrap();
+            if *height >= tree.height {
+                return false;
+            }
         }
+
+        true
     }
 
-    true
-}
+    fn is_visible_from_left(&self, tree: &Tree) -> bool {
+        let row = self.trees.get(tree.y).unwrap();
 
-fn is_visible_from_bottom(grid: &[Vec<u8>], x: usize, y: usize, subject_height: u8) -> bool {
-    for iy in (y + 1)..grid.len() {
-        let height = grid.get(iy).unwrap().get(x).unwrap();
-        if *height >= subject_height {
-            return false;
+        for ix in 0..tree.x {
+            let height = row.get(ix).unwrap();
+            if *height >= tree.height {
+                return false;
+            }
         }
+
+        true
     }
 
-    true
-}
+    fn is_visible_from_right(&self, tree: &Tree) -> bool {
+        let row = self.trees.get(tree.y).unwrap();
 
-fn is_visible_from_left(grid: &[Vec<u8>], x: usize, y: usize, subject_height: u8) -> bool {
-    let row = grid.get(y).unwrap();
-
-    for ix in 0..x {
-        let height = row.get(ix).unwrap();
-        if *height >= subject_height {
-            return false;
+        for ix in (tree.x + 1)..row.len() {
+            let height = row.get(ix).unwrap();
+            if *height >= tree.height {
+                return false;
+            }
         }
+
+        true
     }
-
-    true
-}
-
-fn is_visible_from_right(grid: &[Vec<u8>], x: usize, y: usize, subject_height: u8) -> bool {
-    let row = grid.get(y).unwrap();
-
-    for ix in (x + 1)..row.len() {
-        let height = row.get(ix).unwrap();
-        if *height >= subject_height {
-            return false;
-        }
-    }
-
-    true
 }
